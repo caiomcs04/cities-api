b7cabf61f757da09042a28cf10a861d5f558dc67
caiomcs04 <caio6c@yahoo.com.br>
2021-08-06 11:32:17 -0300

    3#commit(City controller)

--- cities-api/src/main/java/com/backend/citiesapi/controller/CityController.java
+++ cities-api/src/main/java/com/backend/citiesapi/controller/CityController.java
@@ -3,11 +3,11 @@ package com.backend.citiesapi.controller;
 import com.backend.citiesapi.model.City;
 import com.backend.citiesapi.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/cities")
@@ -17,7 +17,7 @@ public class CityController {
     private CityRepository cityRepository;
 
     @GetMapping
-    public List<City> cities(){
-        return cityRepository.findAll();
+    public Page<City> cities(Pageable page){
+        return cityRepository.findAll(page);
     }
 }
--- cities-api/src/main/java/com/backend/citiesapi/model/City.java
+++ cities-api/src/main/java/com/backend/citiesapi/model/City.java
@@ -1,31 +1,46 @@
 package com.backend.citiesapi.model;
 
+import com.backend.citiesapi.util.PointType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.springframework.data.geo.Point;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 @Entity
 @Table(name = "cidade")
 @NoArgsConstructor
 @Getter
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
 public class City {
 
     @Id
     private Long id;
 
-    @Column(name = "uf")
-    private String uf;
+    @Column(name = "nome")
+    private String name;
 
-    @Column(name = "ibge")
-    private String ibge;
+    private Integer uf;
 
-    @Column(name = "sigla")
-    private String code;
+    private Integer ibge;
 
-    private Integer bacen;
+    @Type(type = "point")
+    @Column(name="lat_lon", updatable = false, insertable = false)
+    private Point location;
+
+    private Double longitude;
+
+    private Double latitude;
+
+    private Integer cod_tom;
 
 }
+
